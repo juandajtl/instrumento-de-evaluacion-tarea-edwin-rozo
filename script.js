387da09682b413cf63ff90cd232fb3adea1d5bfc
Kevin <slash2130kevin@gmail.com>
2022-11-04 13:52:35 -0500

    prueba

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let primeraNota = document.getElementById()
-let segundaNota = document.getElementById()
-let terceraNota = document.getElementById();
+let name = document.getElementById('name');
+let primeraNota = document.getElementById.value('Nota1');
+let segundaNota = document.getElementById.value('Nota2');
+let terceraNota = document.getElementById.value('Nota2');
+
+function calcular(){
+let promedio = primeraNota + segundaNota;
+let notaFinal=promedio + terceraNota;
+if (notaFinal>4.5){
+document.write("eres un estudiante exelente, sigue asi")
+}else if (notaFinal>3.5 && notaFinal<4.5){
+    document.write("eres un estudiante muy bueno,aun puedes mejorar")}else if(notaFinal>2 && notaFinal<3.4){
+        document.write("lastimosamente perdiste la materia pero puedes recuperarla :-D")
+    }else if(notaFinal<=2){
+        document.write("lastimosamente perdiste la materia esfuerzate mas para la proxima ಥ_ಥ")
+    }
+}
